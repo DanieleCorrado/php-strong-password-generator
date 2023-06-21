34d14883b7c62a9c15b9337a05520ee6ceda49a7
DanieleCorrado <corrado_daniele@virgilio.it>
2023-06-21 14:26:28 +0200

    add password generator

--- index.php
+++ index.php
@@ -10,9 +10,32 @@
 </head>
 <body>
 
+<form>
+  <label for="pswLength">Lunghezza password: </label>
+  <input type="number" name=lenght>
+  <input type="submit" value="GENERATE">
+</form>
+
+
 <?php 
 
- echo "test";
+  if($_GET != [] || $_GET["pswLength"] != "") {
+    echo generate_password($_GET["lenght"]);
+  } else {
+    echo "inserisci la lunghezza";
+  }
+
+  
+  function generate_password($strength) {
+      $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      $permitted_chars_length = strlen($permitted_chars);
+      $random_string = '';
+      for($i = 0; $i < $strength; $i++) {
+          $random_character = $permitted_chars[mt_rand(0, $permitted_chars_length - 1)];
+          $random_string .= $random_character;
+      }
+      echo $random_string;
+  }
 
 ?>
   
